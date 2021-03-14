3f92b1c3b9cb622c5f0db09965bec1e14222cdf2
Saad Chaouki <sd.chaouki@gmail.com>
2021-03-14 10:26:33 +0000

    Updated lambda function

--- model/lambda/lambda_handler.py
+++ model/lambda/lambda_handler.py
@@ -2,8 +2,10 @@ import boto3
 from urllib.parse import unquote
 
 def lambda_handler(event, context):
+    endpoint_name = 'endpoint name'
+
     runtime = boto3.Session().client('sagemaker-runtime')
-    response = runtime.invoke_endpoint(EndpointName='sagemaker-pytorch-2021-02-21-14-53-45-477',
+    response = runtime.invoke_endpoint(EndpointName=endpoint_name,
                                        ContentType='text/plain',
                                        Body=unquote(event['body'][5:]))
     result = response['Body'].read().decode('utf-8')
